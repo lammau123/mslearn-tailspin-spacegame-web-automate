e59d7995ab83f8c8bb7624493ffcb995b8cc2738
Mau <mlam21@dxc.com>
2020-05-03 23:35:18 +0700

    add features

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 terraform {
   required_version = "> 0.12.0"
-
+  
   backend "azurerm" {
   }
 }
 
 provider "azurerm" {
-  features{}
+  features {}
 }
 
-variable "resource_group_name" { 
+variable "resource_group_name" {
   description = "The name of the resource group"
 }
 
@@ -33,14 +33,14 @@ resource "random_integer" "app_service_name_suffix" {
 }
 
 resource "azurerm_resource_group" "spacegame" {
-  name     = "${var.resource_group_name}"
-  location = "${var.resource_group_location}"
+  name     = var.resource_group_name
+  location = var.resource_group_location
 }
 
 resource "azurerm_app_service_plan" "spacegame" {
-  name                = "${var.app_service_plan_name}"
-  location            = "${azurerm_resource_group.spacegame.location}"
-  resource_group_name = "${azurerm_resource_group.spacegame.name}"
+  name                = var.app_service_plan_name
+  location            = azurerm_resource_group.spacegame.location
+  resource_group_name = azurerm_resource_group.spacegame.name
   kind                = "Linux"
   reserved            = true
 
@@ -52,9 +52,9 @@ resource "azurerm_app_service_plan" "spacegame" {
 
 resource "azurerm_app_service" "spacegame_dev" {
   name                = "${var.app_service_name_prefix}-dev-${random_integer.app_service_name_suffix.result}"
-  location            = "${azurerm_resource_group.spacegame.location}"
-  resource_group_name = "${azurerm_resource_group.spacegame.name}"
-  app_service_plan_id = "${azurerm_app_service_plan.spacegame.id}"
+  location            = azurerm_resource_group.spacegame.location
+  resource_group_name = azurerm_resource_group.spacegame.name
+  app_service_plan_id = azurerm_app_service_plan.spacegame.id
 
   site_config {
     linux_fx_version = "DOTNETCORE|3.1"
@@ -62,11 +62,11 @@ resource "azurerm_app_service" "spacegame_dev" {
   }
 }
 
-output "appservice_name_dev" {
-  value       = "${azurerm_app_service.spacegame_dev.name}"
+output "appservice_name" {
+  value       = azurerm_app_service.spacegame_dev.name
   description = "The App Service name for the dev environment"
 }
 output "website_hostname_dev" {
-  value       = "${azurerm_app_service.spacegame_dev.default_site_hostname}"
+  value       = azurerm_app_service.spacegame_dev.default_site_hostname
   description = "The hostname of the website in the dev environment"
 }
